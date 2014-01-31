3b984ab81749fada4efd297e5401f82afb0eaae6
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-02-01 00:55:12 +0200

    bugfix in build script

--- csrc/libjpeg/build-mingw32.sh
+++ csrc/libjpeg/build-mingw32.sh
@@ -1,11 +1,22 @@
-cat build-mingw32.sh && exit
+which nasm  >/dev/null || { echo "you need nasm"; exit 1; }
+which cmake >/dev/null || { echo "you need cmake"; exit 1; }
 
-need: mingw, cmake, nasm (all in PATH)
+ver=1.3.0
 
-> cd srcdir
-> cmake -G "MSYS Makefiles" .
-> make
+file=libjpeg-turbo-$ver
+[ -f $file.tar.gz ] || {
+	which wget >/dev/null || { echo "you need wget"; exit 1; }
+	wget http://downloads.sourceforge.net/project/libjpeg-turbo/$ver/$file.tar.gz
+}
+[ -d $file ] || tar xvfz $file.tar.gz
+cd $file
 
-copy libjpeg-62.dll to bin/jpeg.dll
+cmake -G "MSYS Makefiles" .
+make clean
+make jpeg
+strip sharedlib/libjpeg-62.dll
+cp sharedlib/libjpeg-62.dll ../../../bin/mingw32/jpeg.dll
 
-strip bin/jpeg.dll
+cd ..
+rm -Rf $file
+rm $file.tar.gz
