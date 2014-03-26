84002c3d8c77c083ebfdc1e4e9058956ef90e1af
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:07:55 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/libjpeg/build-osx64.sh
+++ csrc/libjpeg/build-osx64.sh
@@ -1,8 +1,9 @@
 ./get-it.sh
 cd src
 ./configure --host x86_64-apple-darwin NASM=/opt/local/bin/nasm \
-    CFLAGS="-O3 -m64 -mmacosx-version-min=10.4" LDFLAGS="-m64 -mmacosx-version-min=10.4"
+    CFLAGS="-O3 -m64 -mmacosx-version-min=10.6" LDFLAGS="-m64 -mmacosx-version-min=10.6"
 make clean
 make
 cp -f ".libs/$(readlink .libs/libjpeg.dylib)" ../../../bin/osx64/libjpeg.dylib
+install_name_tool -id @loader_path/libjpeg.dylib ../../../bin/osx64/libjpeg.dylib
 make clean
