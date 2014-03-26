17d6f41d397d25f9fc7463d70ce34d6cf058370a
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 14:19:02 +0200

    libjpeg-turbo sources

--- csrc/libjpeg/get-it.sh
+++ csrc/libjpeg/get-it.sh
@@ -4,11 +4,10 @@ ver=1.3.0
 file=libjpeg-turbo-$ver
 url=http://downloads.sourceforge.net/project/libjpeg-turbo/$ver/$file.tar.gz
 
+[ -d src ] && exit
 [ -f $file.tar.gz ] || {
 	which wget >/dev/null || { echo "you need wget or download $url here"; exit 1; }
 	wget $url
 }
-
-rm -Rf src
 tar xvfz $file.tar.gz
 mv $file src
